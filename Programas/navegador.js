1691c007d5a89a5114c0d5c6cb77d2536f52e78c
Jeffersom <jeffersombenvindo@gmail.com>
2026-08-04 20:36:28 -0300

    Update navegador.js

--- Programas/navegador.js
+++ Programas/navegador.js
@@ -2,11 +2,12 @@
 const iconeNavegador = document.querySelector(".icon-navegador");
 
 iconeNavegador.addEventListener("dblclick", () => {
-    criarJanela(" 🌐 Navegador",
+    criarJanela("🌐 Navegador",
         
         `
         <iframe
-            src="https://www.bing.com/"
+        
+            src="https://cat-bounce.com/message/index.html"
             width="100%"
             height="100%"
             frameborder="0">
